be57dc78f7b0fa77bf21c4e12e26d51b7c8a4e19
Tekin Gunasar <73761503+TekinGunasar@users.noreply.github.com>
2020-11-15 22:58:39 -0800

    Class written for sentiment analysis - current use case is movie reviews, still need WSJ data

--- main.py
+++ main.py
@@ -1,8 +1,53 @@
-from nltk.tokenize import PunktSentenceTokenizer,word_tokenize,sent_tokenize
-from nltk.corpus import state_union
+import nltk
+import random
+from nltk.corpus import movie_reviews
+from nltk.classify import ClassifierI
+from statistics import mode
 
-train_text = state_union.raw("2005-GWBush.txt")
-sample_text = state_union.raw("2006-GWBush.txt")
-custom_sent_tokenizer = PunktSentenceTokenizer.train(train_text)
+class VoteClassifer(ClassifierI):
+    def __init__(self,*classifiers):
+        self._classifiers = classifiers
 
-tokenized = custom_sent_tokenizer.tokenize(sample_text)
+    def classify(self,features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        return mode(votes)
+
+    def confidence(self,features):
+        votes = []
+        for c in self._classifiers():
+            v = c.classify(features)
+            votes.append(v)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes/len(votes)
+        return conf
+
+
+documents = []
+for category  in movie_reviews.categories():
+    for fileid in movie_reviews.fileids(category):
+        documents.append(list((movie_reviews.words(fileid),category)))
+
+random.shuffle(documents)
+all_words = []
+for w in movie_reviews.words():
+    all_words.append(w.lower())
+
+word_features = list(all_words.keys())[:3000]
+
+def find_features(document):
+    words = set(document)
+    features = {}
+    for words in word_features:
+        features[w] = (w in words)
+
+    return features
+
+featuresets = [(find_features(rev),category) for(rev,category) in documents]
+training_set = featuresets[:1900]
+testing_set = featuresets[1900:]
+
